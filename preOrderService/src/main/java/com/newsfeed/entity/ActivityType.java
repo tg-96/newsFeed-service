992a0d0a0677d2cba77dc9231db311d3248e6087
Jeong tae gyu <jtg960528@gmail.com>
2024-02-01 00:55:56 +0900

    Refactor: domain별로 구조 변경

--- preOrderService/src/main/java/com/newsfeed/entity/ActivityType.java
+++
@@ -1,5 +0,0 @@
-package com.newsfeed.entity;
-
-public enum ActivityType {
-    FOLLOWS,COMMENT_LIKE,COMMENTS,POSTS,POST_LIKES
-}
---
+++ preOrderService/src/main/java/com/preOrderService/newsFeed/dto/PostLikesDto.java
@@ -0,0 +1,13 @@
+package com.preOrderService.newsFeed.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostLikesDto {
+    private Long PostId;
+    private String likersEmail;
+}
